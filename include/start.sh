5bba84e549bbe5bcdb459d871458251b96c15aa3
Your Name <you@example.com>
2025-07-13 14:47:09 -0400

    not helpful commit message

--- include/start.sh
+++ include/start.sh
@@ -2,10 +2,15 @@
 
 # Pcap analysis tools need to disable name resolution as these tools in webvm will literally
 # attempt to resolve names from 8.8.8.8 without a network interface.
-alias tcpdump='tcpdump -n'
-alias tshark='tshark -n'
+echo "alias tcpdump='tcpdump -n'" >> /home/user/.bashrc
+echo "alias tshark='tshark -n'" >> /home/user/.bashrc
+
+echo "export PATH=$PATH:/opt/zeek/bin" >> /home/user/.bashrc
 
 # Using awk to print a new quote upon a new instance of webvm.
 sort -R /home/user/quotes.txt | head -n1 | awk '"intro.txt"==ARGV[1] { print } "-"==ARGV[2] { gsub(/(\\\\)/, "\n   "); print } END{ print "\n" }' /home/user/intro.txt - > /home/user/banner
 rm /home/user/quotes.txt /home/user/intro.txt
 cat banner
+
+# self-delete start.sh
+rm -- "$0"
